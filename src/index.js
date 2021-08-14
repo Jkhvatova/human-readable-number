23a24e9ed692db591c65b82631d8546a5e0fa709
Julia Khvatova <jkhvatova@gmail.com>
2021-08-15 01:00:21 +0300

    init: start task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    function convertHundreds(number) {
+        if (number > 99) {
+            return (
+                ones[Math.floor(number / 100)] +
+                " hundred " +
+                convertTens[num % 100]
+            );
+        } else {
+            return convertTens(number);
+        }
+    }
+
+    function convertTens(number) {
+        if (number < 20) {
+            return tens[number];
+        } else {
+            return tens[Math.floor(number / 10)] + " " + ones[num % 10];
+        }
+    }
+};
